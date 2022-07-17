20b508354c0c3ca4d8df8e4b3c0b858328b598ec
David Arustamyan <david.arustamyan.96@gmail.com>
2022-07-17 15:53:33 +0200

    done the navbar, sidbar cart, sidbar login, home section and scroll for header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log('Hello World');
+// HOME SWIPER
+
+const homeSwiper = new Swiper('.home-swiper', {
+  spaceBetween: 30,
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+// CHANGE BACKGROUND HEADER
+
+function scrollHeader() {
+  const header = document.getElementById('header');
+  //when the scroll is greater than 50 viewport height, add the scroll-header to the header tag
+  if (this.scrollY >= 50) {
+    header.classList.add('scroll-header');
+  } else {
+    header.classList.remove('scroll-header');
+  }
+}
+window.addEventListener('scroll', scrollHeader);
